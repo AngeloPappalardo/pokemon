22d5fb54bb02ab82b3c12803a0a3c8e95c0b697a
AngeloPappalardo <angelo961996@gmail.com>
2022-12-28 15:35:27 +0100

    pokedex
    ultime modifiche

--- script.js
+++ script.js
@@ -1,11 +1,17 @@
 const pokemonName = document.querySelector(".pokemon__name");
 const pokemonNumber = document.querySelector(".pokemon__number");
 const pokemonImage = document.querySelector(".pokemon__image");
-const pokemonType = document.querySelector(".types")
+const pokemonType = document.querySelector(".types");
+const pokemonType2 = document.querySelector(".type");
+const typeContainer = document.querySelector(".types__container");
 const form = document.querySelector(".form");
 const input = document.querySelector(".input__serch");
 const buttonPrev = document.querySelector(".btn-prev");
 const buttonNext = document.querySelector(".btn-next");
+const hpPokemon = document.querySelector(".hp");
+const attackPokemon = document.querySelector(".attack");
+const defensePokemon = document.querySelector(".defense");
+const displayDamage = document.querySelector(".damage");
 
 let serchPokemon = 1;
 
@@ -21,20 +27,29 @@ const fetchPokemon = async (pokemon) => {
 
 const renderPokemon = async (pokemon) => {
   pokemonName.innerHTML = "Loading...";
+  typeContainer.style.display = "none";
+  displayDamage.style.display = "none";
   pokemonNumber.innerHTML = " ";
+  pokemonType2.innerHTML = " ";
   const data = await fetchPokemon(pokemon);
 
   if (data) {
+    typeContainer.style.display = "flex";
     pokemonImage.style.display = "block";
+    displayDamage.style.display = "block";
     pokemonName.innerHTML = data.name;
     pokemonNumber.innerHTML = data.id;
-    pokemonType.innerHTML = data["types"]["0"]["type"]["name"];
     pokemonImage.src =
       data["sprites"]["versions"]["generation-v"]["black-white"]["animated"][
         "front_default"
       ];
     input.value = "";
     serchPokemon = data.id;
+    hpPokemon.innerHTML= data["stats"][0]["base_stat"];
+    attackPokemon.innerHTML= data["stats"][1]["base_stat"];
+    defensePokemon.innerHTML = data["stats"][2]["base_stat"];
+    pokemonType.innerHTML = data["types"][0]["type"]["name"];
+    pokemonType2.innerHTML = data["types"][1]["type"]["name"];
   } else {
     pokemonImage.style.display = "none";
     pokemonName.innerHTML = "Not found :c";
